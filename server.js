bb3fc1b64ee169d76847f17e11c16acf0ba7bc44
SayantanStackflow <sayantan.college6291@gmail.com>
2023-04-21 21:13:47 +0100

    added some comments

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const express= require('express')
-const request= require('request')
 const bodyParser=require('body-parser')
 const https= require('https')
 
-
 const app=express()
-
+//set bodyparese to get the data from html
 app.use(bodyParser.urlencoded({extended: true}))
 
+//used to set the public folder static to load the css and imgs
 app.use('/public', express.static('public'));
 
+//basic get request to the main html page
 app.get('/', function(req,res){
     res.sendFile(__dirname+ '/index.html')
 })
 
+//handle the post req from the html from
 app.post('/', function(req,res){
     const name= req.body.fullname;
     const emailID= req.body.email;
@@ -29,14 +30,19 @@ app.post('/', function(req,res){
 
         ]
     };
+
+    //data in a single line format
     const jsonData= JSON.stringify(data)
 
+    //api route page
     const url='https://us21.api.mailchimp.com/3.0/lists/e67bff0159'
+
     const options={
         method: 'POST',
         auth: "subscription:226c2c680be31b6618cfa9dcffde3162-us21"
     }
 
+    //after submitting form redirect
     const request= https.request(url, options, function(response){
 
         if(response.statusCode ==200){
@@ -50,12 +56,11 @@ app.post('/', function(req,res){
         })
     })
 
-    
-
     request.write(jsonData);
     request.end();
 })
 
+// back to the main html page
 app.post("/failure", function(req,res){
     res.redirect('/')
 })
@@ -68,6 +73,5 @@ app.listen(3000, function(req,res){
 // api key:
 
 // 226c2c680be31b6618cfa9dcffde3162-us21
-
 
 //e67bff0159 
